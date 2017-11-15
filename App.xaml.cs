bf45af64c6acc10a9096df517b757d866b62dc7e
Peter Meyer <ptr_meyer@hotmail.com>
2017-11-15 12:54:35 +0100

    added reader for arguments to project

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,9 +13,23 @@
         {
             Console.WriteLine("Starting app");
 
+
             // bool indicating if the program should start a GUI showing Kinect images(true), 
             //or only send tracked data via UDP(false)
-            bool showWindow = true;
+            bool showWindow = false;
+
+            for (int i = 0; i != e.Args.Length; ++i)
+            {
+                Console.WriteLine(e.Args[i]);
+                if (e.Args[i] == "-s")
+                {
+
+                    showWindow = true;
+                }
+            }
+
+
+
 
             // create new kinectData object to processes frames from the Kinect camera
             KinectData kinectData = new KinectData(showWindow);
